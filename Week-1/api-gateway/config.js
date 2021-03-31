83ed79515d2ab32a9e28d2dae7ecda4a759d0031
Jimmy Pinson <jpinson@my365.bellevue.edu>
2021-03-30 20:26:01 -0500

    Update config.js

--- Week-1/api-gateway/config.js
+++ Week-1/api-gateway/config.js
@@ -1,7 +1,19 @@
+/*
+============================================
+; Title:  config.js
+; Author: James Pinson
+; Date: 3/30/2021
+; Description: This is our configure javascript page. 
+;===========================================
+*/
+
+//This sets up our config variable. 
 var config = {};
+
 
 config.web = {};
 
+//This sets the web port to 3000.
 config.web.port = process.env.PORT || '3000';
 
 
