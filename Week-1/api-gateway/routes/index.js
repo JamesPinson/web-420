87dbd78bce3ff706d1ae878ee35b1b5107b0c8bb
Jimmy Pinson <jpinson@my365.bellevue.edu>
2021-03-30 20:27:04 -0500

    Update index.js

--- Week-1/api-gateway/routes/index.js
+++ Week-1/api-gateway/routes/index.js
@@ -1,3 +1,13 @@
+/*
+============================================
+; Title:  Index.js
+; Author: James Pinson
+; Date: March 30 2021
+; Description: This is our index.js page.
+;===========================================
+*/
+
+//This is our require statements.
 var express = require('express');
 var router = express.Router();
 
